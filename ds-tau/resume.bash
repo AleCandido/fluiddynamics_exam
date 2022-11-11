ff5228a3a3e3a81ef9923e01eedd1ef3b568e49e
Alessandro Candido <candido.ale@gmail.com>
2022-11-11 23:50:50 +0100

    Complete resume script

--- ds-tau/resume.bash
+++ ds-tau/resume.bash
@@ -1,11 +1,31 @@
-LATEST_FULL_DUMP=$(realpath $1)
-export RUNDIR=$(dirname $LATEST_FULL_DUMP)
+LAST_FULL_DUMP=$(realpath $1)
+export RUNDIR=$(dirname $LAST_FULL_DUMP)
 
 source $(dirname ${BASH_SOURCE[0]})/_setup.bash
 
 # -----------------------------------------------------------------------------
 
-sed -e 's/dustydisc_0[[:digit:]]\{4\}/'"$(basename $LATEST_FULL_DUMP)"'/' dustydisc.in -i
+shopt -s extglob
+
+# extract numbers for last and last full dump
+lastfull=$(echo $LAST_FULL_DUMP | rev | cut -b -5 | rev)
+last=$(ls -1 $RUNDIR/dustydisc_+([[:digit:]]) | tail -1 | rev | cut -b -5 | rev)
+
+# replace last full dump in .in file
+sed -e 's/dustydisc_[[:digit:]]\{5\}/'"$(basename $LAST_FULL_DUMP)"'/' dustydisc.in -i
+
+# save all dumps more recent than last full, and leave the slot
+for n in $(seq $(expr $lastfull + 1) $last); do
+  # construct old and new name
+  padded=$(printf "%05d" $n)
+  old=$RUNDIR/dustydisc_$padded
+  new=$RUNDIR/dustydisc_old_$padded
+
+  # rename
+  mv $old $new
+done
+
+# restart
 ./phantom dustydisc.in
 
 # -----------------------------------------------------------------------------
