4ca2596b6a078d30f09f52fc6943d880152c3e04
Alessandro Candido <candido.ale@gmail.com>
2022-11-12 02:14:02 +0100

    Fix pull for multiple and new remotes

--- ds-tau/_pull.bash
+++ ds-tau/_pull.bash
@@ -3,10 +3,16 @@ source $(dirname ${BASH_SOURCE[0]})/_setup.bash
 
 REMOTE=phantomblot
 REMOTEFOLDER='~/cave/Projects/Misc/fluiddynamics_exam/_runs/ds-tau-1'
-DUMPDIR=$(dirname $RUNDIR)/_results@$PROJECT
+# REMOTE=galileo
+# REMOTEFOLDER='~/fluiddynamics_exam/_runs/ds-tau-32'
+DUMPDIR=$(dirname $RUNDIR)/_results@$PROJECT@$REMOTE
 mkdir -p $DUMPDIR
 
-last=$(ls -1 $DUMPDIR/dustydisc_0* | tail -1 | rev | cut -b -5 | rev)
+if [ -f $DUMPDIR/dustydisc_00000 ]; then
+  last=$(ls -1 $DUMPDIR/dustydisc_0* | tail -1 | rev | cut -b -5 | rev)
+else
+  last=0
+fi
 
 for i in $(seq -f "%05g" $last 400); do
   scp $REMOTE:$REMOTEFOLDER/dustydisc_$i $DUMPDIR/
