80fe46c0927b55033b7bafb538e241e66a082a41
Alessandro Candido <candido.ale@gmail.com>
2022-11-14 17:29:24 +0100

    Try with gas only disc

--- ds-tau/disc.bash
+++ ds-tau/disc.bash
@@ -6,16 +6,16 @@ fi
 source $(dirname ${BASH_SOURCE[0]})/_setup.bash
 
 # -----------------------------------------------------------------------------
-$SRCDIR/../phantom/scripts/writemake.sh dustydisc >Makefile
+$SRCDIR/../phantom/scripts/writemake.sh disc >Makefile
 make
 make setup
 
 # copy prepared configuration
-cp $SRCDIR/dustydisc-v3.setup $RUNDIR/dustydisc.setup
+cp $SRCDIR/disc.setup $RUNDIR/dustydisc.setup
 ./phantomsetup dustydisc.setup
 
 # test run - to check star relaxation
-cp $SRCDIR/dustydisc-v4.in $RUNDIR/dustydisc.in
+# cp $SRCDIR/dustydisc-v4.in $RUNDIR/dustydisc.in
 ./phantom dustydisc.in
 # -----------------------------------------------------------------------------
 
